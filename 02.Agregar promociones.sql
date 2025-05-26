3fb78b4813b42375707252c6b9d093ba28decded
NataliaSC27 <scnatalia544@gmail.com>
2025-05-26 13:45:12 -0500

    02.Agregar promociones.sql

---
+++ 02.Agregar promociones.sql
@@ -0,0 +1,6 @@
+INSERT INTO promociones (nombre, descripcion, tipo, valor, fecha_inicio, fecha_fin) VALUES
+  ('Descuento frecuente', 'Al momento de pagar su pasaje se le dará una tarifa menor por ser un usuario frecuente', 'DESCUENTO', 500,    '2023-05-01', '2024-06-30'),
+  ('Subsidio educativo',   'Subsidio para estudiantes inscritos en programas oficiales',                        'BONUS',      30000, '2023-04-15', '2024-06-30'),
+  ('Promoción estudiantil','Descuento por ser estudiante universitario',                                    'DESCUENTO',  200,    '2023-06-01', '2023-07-15'),
+  ('Bono por fidelidad',   'Bono adicional por ser cliente frecuente',                                     'BONUS',      50000, '2025-05-10', '2025-07-10'),
+  ('Apoyo social adulto mayor','Subsidio económico exclusivo para personas mayores de 60 años con tarjeta estándar, como apoyo al transporte público.', 'OTRO', 30000, '2022-03-01', '2022-09-30');

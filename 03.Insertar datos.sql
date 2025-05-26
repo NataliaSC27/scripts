66427413254070add78f0fb61cd311152eabf93f
NataliaSC27 <scnatalia544@gmail.com>
2025-05-26 16:20:52 -0500

    03.Insertar datos.sql

---
+++ 03.Insertar datos.sql
@@ -0,0 +1,14 @@
+--Inserta datos en tabla validaciones 
+INSERT INTO validaciones (validacion_id, viaje_id, dispositivo_id, fecha) VALUES
+(1, 77420, 2, '2024-01-05'),
+(2, 81746, 3, '2024-01-05'),
+(3, 81785, 1, '2024-01-05'),
+(4, 121448, 2, '2024-01-05');
+
+--Inserta datos en tabla de dispositivos 
+INSERT INTO dispositivos (dispositivo_id, tipo, fecha_instalacion) VALUES
+(1, 'Torniquete', '2023-01-15'),
+(2, 'Validador móvil', '2023-03-10'),
+(3, 'Lector QR', '2024-02-20');
+
+ 

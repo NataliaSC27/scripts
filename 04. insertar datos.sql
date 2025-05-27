6076946bb5801afe7bd9c0c8a424fcd1e575c050
NataliaSC27 <scnatalia544@gmail.com>
2025-05-26 19:04:40 -0500

    04. insertar datos.sql

---
+++ 04. insertar datos.sql
@@ -0,0 +1,29 @@
+INSERT INTO medios_pago (nombre) VALUES
+('Efectivo'),
+('Tarjeta débito'),
+('Tarjeta crédito'),
+('Billetera digital'),
+('Transferencia bancaria');
+
+UPDATE recargas SET medio_id = 3 WHERE recarga_id = 2;
+UPDATE recargas SET medio_id = 5 WHERE recarga_id = 3;
+UPDATE recargas SET medio_id = 1 WHERE recarga_id = 11;
+UPDATE recargas SET medio_id = 4 WHERE recarga_id = 12;
+UPDATE recargas SET medio_id = 2 WHERE recarga_id = 21;
+UPDATE recargas SET medio_id = 3 WHERE recarga_id = 23;
+UPDATE recargas SET medio_id = 5 WHERE recarga_id = 32;
+UPDATE recargas SET medio_id = 1 WHERE recarga_id = 34;
+UPDATE recargas SET medio_id = 2 WHERE recarga_id = 36;
+UPDATE recargas SET medio_id = 4 WHERE recarga_id = 43;
+UPDATE recargas SET medio_id = 3 WHERE recarga_id = 44;
+UPDATE recargas SET medio_id = 1 WHERE recarga_id = 54;
+UPDATE recargas SET medio_id = 2 WHERE recarga_id = 56;
+UPDATE recargas SET medio_id = 4 WHERE recarga_id = 65;
+UPDATE recargas SET medio_id = 5 WHERE recarga_id = 67;
+UPDATE recargas SET medio_id = 1 WHERE recarga_id = 74;
+UPDATE recargas SET medio_id = 3 WHERE recarga_id = 87;
+UPDATE recargas SET medio_id = 2 WHERE recarga_id = 90;
+UPDATE recargas SET medio_id = 4 WHERE recarga_id = 112;
+UPDATE recargas SET medio_id = 5 WHERE recarga_id = 113;
+UPDATE recargas SET medio_id = 1 WHERE recarga_id = 122;
+UPDATE recargas SET medio_id = 2 WHERE recarga_id = 133;
